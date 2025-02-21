fd2d6e2fc32ebe5c87b74b56ab62c1325b22e055
Igor Henrique <61789282+igorhgds@users.noreply.github.com>
2025-02-21 15:59:49 -0300

    conclusao da add de cliente e os metodos(sacar, depositar, transferir e imprimirExtrato

--- src/ContaPoupanca.java
+++ src/ContaPoupanca.java
@@ -1,2 +1,12 @@
-public class ContaPoupanca {
+public class ContaPoupanca extends Conta {
+
+    public ContaPoupanca(Cliente cliente) {
+        super(cliente);
+    }
+
+    @Override
+    public void imprimirExtrato() {
+        System.out.println("=== Extrato Conta Poupança ===");
+        super.imprimirInfosComuns();
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,17 @@
 public class Main {
+
+    public static void main(String[] args) {
+        Cliente venilton = new Cliente();
+        venilton.setNome("Venilton");
+
+        Conta cc = new ContaCorrente(venilton);
+        Conta poupanca = new ContaPoupanca(venilton);
+
+        cc.depositar(100);
+        poupanca.depositar(100);
+
+        cc.imprimirExtrato();
+        poupanca.imprimirExtrato();
+    }
+
 }
